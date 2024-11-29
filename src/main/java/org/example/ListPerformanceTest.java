fc934c8d2259f356bdee4501916a1fd0c18410cb
ksushapopova <ks.popova.2005@mail.ru>
2024-11-30 01:51:39 +0300

    Testing the removal and receipt of items

--- src/main/java/org/example/ListPerformanceTest.java
+++ src/main/java/org/example/ListPerformanceTest.java
@@ -20,9 +20,24 @@ public class ListPerformanceTest
         long endTime = System.nanoTime();
         long addTime = endTime - startTime;
 
+        startTime = System.nanoTime();
+        for (int i = 0; i < TEST_SIZE; i++) {
+            list.get(i);
+        }
+        endTime = System.nanoTime();
+        long getTime = endTime - startTime;
+
+        startTime = System.nanoTime();
+        for (int i = TEST_SIZE - 1; i >= 0; i--) {
+            list.remove(i);
+        }
+        endTime = System.nanoTime();
+        long removeTime = endTime - startTime;
+
         System.out.printf("%s:\n", listType);
         System.out.printf("  add: %d ms\n", addTime / 100000);
-
+        System.out.printf("  get: %d ms\n", getTime / 100000);
+        System.out.printf("  remove: %d ms\n", removeTime / 100000);
         System.out.println();
     }
 }
